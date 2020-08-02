9ee1b49c8e10d3fea43fd7eab5ba5f128b6a6b18
Skida12138 <su1q2d2@163.com>
2020-08-02 19:38:37 +0800

    add: 添加顶栏组件

---
+++ src/components/TopBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-layout">
+    <div class="left-actions">
+      <slot name="left-actions"></slot>
+    </div>
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="right-actions">
+      <slot name="right-actions"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.main-layout {
+  background-color: #606060;
+  color: white;
+  font-size: 18px;
+  display: flex;
+  .left-actions {
+    display: flex;
+    align-items: center;
+  }
+  .right-actions {
+    display: flex;
+    align-items: center;
+  }
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+}
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'top-bar',
+})
+export default class TopBar extends Vue {}
+</script>
